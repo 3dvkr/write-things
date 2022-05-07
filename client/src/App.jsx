0c2337da6d20cd52f53942abfe12ea6126336c9c
dvkr <v3dvkr@gmail.com>
2022-05-07 08:34:34 -0600

    added up Notion login (form and route)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 
+// const oauth_client_id = ***;
 function App() {
   const [notes, setNotes] = useState("")
+
+  useEffect(() => {
+    const params = new URL(window.document.location).searchParams;
+
+    const code = params.get("code");
+    console.log({ code });
+    if (!code) return;
+    fetch(`http://localhost:4000/login/${code}`)
+      .then((resp) => resp.json())
+  }, []);
+
   return (
     <div>
+      <a href={`https://api.notion.com/v1/oauth/authorize?client_id=${oauth_client_id}&response_type=code&owner=user`}>Connect To Notion</a>
       <form onSubmit={(e) => {
         e.preventDefault()
         console.log(notes)
--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
+const { Client } = require("@notionhq/client");
+
+require("dotenv").config();
+const notionClientId = process.env.NOTION_CLIENT_ID;
+const notionClientSecret = process.env.NOTION_CLIENT_SECRET;
+
 const app = express();
 
 // Middleware
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
@@ -13,7 +20,21 @@ app.get("/", (req, res) => {
 app.post("/notes", (req, res) => {
     console.log(req.body)
     res.send()
-})
+});
+
+app.get("/login/:code", async (req, res) => {
+    const { code } = req.params;
+    console.log(code);
+    const resp = await axios({
+        method: "POST",
+        url: "https://api.notion.com/v1/oauth/token",
+        auth: { username: notionClientId, password: notionClientSecret },
+        headers: { "Content-Type": "application/json" },
+        data: { code, grant_type: "authorization_code" },
+      });
+    console.log(resp.data);
+    res.json("login");
+});
 
 app.listen(4000, () => {
     console.log("Server is running on port 4000");

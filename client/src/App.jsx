5f7abe2d328bcbca05ff9685a0fe157a2bf6ba95
dvkr <v3dvkr@gmail.com>
2022-05-30 07:54:00 -0600

    added localStorage (e.g. for logged out users)

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,26 +2,34 @@ import { useState, useEffect } from 'react'
 import Form from "./components/Form"
 // const oauth_client_id = ***;
 function App() {
-  const [notes, setNotes] = useState("")
-  const [pages, setPages] = useState([])
+  const [isLoggedIn, setIsLoggedIn] = useState(!!document.cookie);
+  const [pages, setPages] = useState(
+    JSON.parse(localStorage.getItem("page-list")) || []
+  );
 
   useEffect(() => {
+    setPages(JSON.parse(localStorage.getItem("page-list")) || []);
     const params = new URL(window.document.location).searchParams;
 
     const code = params.get("code");
     console.log({ code });
-    if (!code) return;
+    if (!code || isLoggedIn) return;
     fetch(`http://localhost:4000/login/${code}`, {
       credentials: "include",
     })
       .then((resp) => resp.json())
-      .then(titles => setPages(titles))
+      .then(titles =>{
+        localStorage.setItem("page-list", JSON.stringify(titles));
+        setIsLoggedIn(true);
+        setPages(titles)
+      }
+        )
   }, []);
 
   return (
     <div>
       <a href={`https://api.notion.com/v1/oauth/authorize?client_id=${oauth_client_id}&response_type=code&owner=user`}>Connect To Notion</a>
-      <Form pages={pages}/>
+      <Form pages={pages} isLoggedIn={isLoggedIn} />
     </div>
   )
 }
--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -8,8 +8,10 @@ import {
   TextField,
 } from "@mui/material";
 
-const Form = ({ pages }) => {
-  const [notes, setNotes] = useState("");
+const Form = ({ pages, isLoggedIn }) => {
+  const [notes, setNotes] = useState(
+    JSON.parse(localStorage.getItem("write-notes")) || ""
+  );
   const [pageName, setPageName] = useState("");
   const [memo, setMemo] = useState("");
 
@@ -37,51 +39,58 @@ const Form = ({ pages }) => {
         padding: "1rem",
       }}
     >
+      {isLoggedIn && (
+        <FormControl>
+          <InputLabel htmlFor="pageName">
+            Choose where to save your writing:
+          </InputLabel>
+          <NativeSelect
+            label="Where to save your writing:"
+            name="pageName"
+            value={pageName}
+            id="pageName"
+            onChange={(e) => setPageName(e.target.value)}
+          >
+            {pages.map((page) => (
+              <option value={page} key={page}>
+                {page}
+              </option>
+            ))}
+          </NativeSelect>
+        </FormControl>
+      )}
+      {isLoggedIn && (
+        <TextField
+          label="Title/Memo"
+          name="memo"
+          placeholder="Title/Memo"
+          onChange={(e) => {
+            setMemo(e.target.value);
+          }}
+          value={memo}
+        />
+      )}
       <FormControl>
-        <InputLabel htmlFor="pageName">
-          Choose where to save your writing:
-        </InputLabel>
-        <NativeSelect
-          label="Where to save your writing:"
-          name="pageName"
-          value={pageName}
-          id="pageName"
-          onChange={(e) => setPageName(e.target.value)}
-        >
-          {pages.map((page) => (
-            <option value={page} key={page}>
-              {page}
-            </option>
-          ))}
-        </NativeSelect>
+        <TextField
+          label="Notes"
+          multiline={true}
+          rows={6}
+          width="100%"
+          variant="outlined"
+          name="notes"
+          placeholder="Notes"
+          value={notes}
+          onChange={(e) => {
+            localStorage.setItem("write-notes", JSON.stringify(notes));
+            setNotes(e.target.value);
+          }}
+        />
       </FormControl>
-      <TextField
-        label="Title/Memo"
-        name="memo"
-        placeholder="Title/Memo"
-        onChange={(e) => {
-          setMemo(e.target.value);
-        }}
-        value={memo}
-      />
-      <FormControl>
-      <TextField
-        label="Notes"
-        multiline={true}
-        rows={6}
-        width="100%"
-        variant="outlined"
-        name="notes"
-        placeholder="Notes"
-        value={notes}
-        onChange={(e) => {
-          setNotes(e.target.value);
-        }}
-      />
-      </FormControl>
-      <Button variant="contained" type="submit">
-        Send to Notion
-      </Button>
+      {isLoggedIn && (
+        <Button variant="contained" type="submit">
+          Send to Notion
+        </Button>
+      )}
     </Box>
   );
 };

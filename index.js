5240159f3d65bd041f58cddf23e1e4b39f122828
dvkr <v3dvkr@gmail.com>
2022-06-04 22:44:51 -0600

    deployment changes

--- index.js
+++ index.js
@@ -1,21 +1,31 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const path = require("path");
 
-const { getUserInfo, postToNotion, logOut } = require('./controllers/notion');
+const { getUserInfo, postToNotion, logOut } = require("./controllers/notion");
 
+const PORT = process.env.PORT || 4000;
 const app = express();
 app.use(express.json());
-app.use(cors({
+app.use(
+  cors({
     credentials: true,
-    origin: 'http://localhost:3000'
-}));
+    origin: "*",
+  })
+);
 app.use(cookieParser());
+
+app.use(express.static(path.join(__dirname, 'client/dist')));
 
 app.get("/login/:code", getUserInfo);
 app.post("/notes", postToNotion);
-app.delete("/logout", logOut)
+app.delete("/logout", logOut);
 
-app.listen(4000, () => {
-    console.log("Server is running on port 4000");
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/public/index.html"));
+  });
+
+app.listen(PORT, () => {
+  console.log("Server is running on port ", PORT);
 });
